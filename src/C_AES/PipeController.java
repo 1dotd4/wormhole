9cdc462204a190b2ecec94a17c55fa4a966d8916
unpx <d4@unpx.net>
2022-12-02 11:47:13 +0100

    Fix test and add script to run

--- src/C_AES/PipeController.java
+++ src/C_AES/PipeController.java
@@ -62,7 +62,7 @@ public class PipeController {
   public static void main(String[] args){
     PipeController p = new PipeController("test");
     byte[] buf = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-    byte[] iv = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    byte[] iv =  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
     byte[] key = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
     p.set_key(key);
     p.set_vec(iv);
